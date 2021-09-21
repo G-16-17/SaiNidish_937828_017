6c077980de6e0058e15182d078091a51208422d7
msnidish <66253048+msnidish@users.noreply.github.com>
2021-09-21 08:24:00 +0530

    Add files via upload

---
+++ AWS/Swagger Hands on/swagger-hands on/src/main/java/com/learn/swaggerdemo/SwaggerDemoApplication.java
@@ -0,0 +1,17 @@
+package com.learn.swaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information"))
+public class SwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDemoApplication.class, args);
+	}
+
+}
